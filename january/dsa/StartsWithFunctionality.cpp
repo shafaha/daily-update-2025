8e9b7a62c2bda8da497b737f8dcde49516f89b4a
shafa-at-hussain <shafex1198@gmail.com>
2025-01-09 19:58:39 +0530

    jan-9

---
+++ january/dsa/StartsWithFunctionality.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/?envType=daily-question&envId=2025-01-08
+
+class Solution
+{
+public:
+    int countPrefixSuffixPairs(vector<string> &words)
+    {
+        int total = 0;
+        for (int i = 0; i < words.size(); ++i)
+        {
+            for (int j = i + 1; j < words.size(); ++j)
+            {
+                if (words[i].size() > words[j].size())
+                    continue;
+                int x = words[i].size();
+                int y = words[j].size();
+                // cout<<words[i]<<" "<<words[j].substr(y-x, x)<<endl;
+                if (words[j].substr(0, x) == words[i] && words[j].substr(y - x, x) == words[i])
+                    ++total;
+            }
+        }
+
+        return total;
+    }
+};
